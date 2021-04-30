30d418750942399b4095d5b8f17ae744ef310197
Jomkie <937574740@qq2.com>
2021-04-30 11:27:23 +0800

    测试文件

--- jomkie-test/src/main/java/com/jomkie/test/TestOne.java
+++ jomkie-test/src/main/java/com/jomkie/test/TestOne.java
@@ -1,10 +1,23 @@
 package com.jomkie.test;
 
-import com.jomkie.common.BaseResponse;
+import com.jomkie.model.JoUser;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TestOne {
 
     public static void main(String[] args) {
-        System.out.println(BaseResponse.PARAM_ERROR.toString());
+
+        List<JoUser> list = IntStream.range(0, 21)
+                .mapToObj(i ->
+                    new JoUser(Long.valueOf(i), "李寻欢" + i, i % 5, "明朝" + i)
+                ).collect(Collectors.toList());
+
+        Map<Integer, JoUser> map = list.stream().collect(Collectors.toMap(JoUser::getAge, v -> v, (x, y) -> x));
+        Map<Integer, Integer> map2 = list.stream().collect(Collectors.toMap(JoUser::getAge, JoUser::getAge, Integer::sum));
+
     }
 }
