8913f1e8727d6f4d9393af20c7e4e03671c9e74c
Jomkie <937574740@qq2.com>
2021-05-20 16:35:39 +0800

    修改远程请求方法

--- jomkie-test/src/main/java/com/jomkie/common/remote/RemoteApi.java
+++ jomkie-test/src/main/java/com/jomkie/common/remote/RemoteApi.java
@@ -1,17 +1,17 @@
 package com.jomkie.common.remote;
 
 import com.alibaba.fastjson.JSONObject;
+import com.jomkie.common.LemonException;
 import com.jomkie.common.RequestObj;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Jomkie
@@ -39,11 +39,18 @@ public class RemoteApi {
 
         HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-
         HttpEntity<JSONObject> requestEntity = new HttpEntity<>(requestDataJsonObj, requestHeaders);
-        Object result = restTemplate.postForObject(url, requestEntity, Object.class);
+
+        ResponseEntity<Object> responseEntity = restTemplate.postForEntity(url, requestEntity, Object.class);
+        if (Objects.isNull(responseEntity)) { throw new LemonException("远程请求微信异常"); }
+        if ( ! Objects.equals(responseEntity.getStatusCodeValue(), HttpStatus.OK.value())) {
+            throw new LemonException("请求不成功，异常码为：{}" + responseEntity.getStatusCodeValue());
+        }
+
+        Object result = responseEntity.getBody();
         String resultJsonStr = JSONObject.toJSONString(result);
         log.info("微信请求结果值为：{}", resultJsonStr);
+
         return JSONObject.toJSONString(resultJsonStr);
     }
 
