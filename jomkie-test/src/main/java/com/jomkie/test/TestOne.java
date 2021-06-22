35cd76d2fe2a3333b8bbe1b36ae91a016709fa9e
Jomkie <937574740@qq2.com>
2021-06-22 16:09:31 +0800

    增加方法

--- jomkie-test/src/main/java/com/jomkie/test/TestOne.java
+++ jomkie-test/src/main/java/com/jomkie/test/TestOne.java
@@ -2,6 +2,10 @@ package com.jomkie.test;
 
 import com.jomkie.model.JoUser;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,10 +13,13 @@ import java.util.stream.IntStream;
 public class TestOne {
 
     public static void main(String[] args) {
+        test01();
+    }
 
+    public void test00() {
         List<JoUser> list = IntStream.range(0, 21)
                 .mapToObj(i ->
-                    new JoUser(Long.valueOf(i), "李寻欢" + i, i % 5, "明朝" + i)
+                        new JoUser(Long.valueOf(i), "李寻欢" + i, i % 5, "明朝" + i)
                 ).collect(Collectors.toList());
 
         Map<Integer, JoUser> map = list.stream().collect(Collectors.toMap(JoUser::getAge, v -> v, (x, y) -> x));
@@ -21,8 +28,8 @@ public class TestOne {
                 Collectors.groupingBy(
                         JoUser::getAge,
                         Collectors.collectingAndThen(
-                            Collectors.maxBy(Comparator.comparing(JoUser::getAge)),
-                            Optional::get
+                                Collectors.maxBy(Comparator.comparing(JoUser::getAge)),
+                                Optional::get
                         )
                 )
         );
@@ -40,8 +47,29 @@ public class TestOne {
                         Collectors.mapping(u -> u.getAge() < 10 ? "small" : "big", Collectors.toSet())
                 )
         );
+    }
+
+    public static void test01() {
+        LocalDate.of(2021, 6, 21);
+        LocalTime.of(22, 22, 22);
+
+        LocalDate localDate = LocalDate.parse("2021-06-21");
+        LocalTime localTime = LocalTime.parse("22:22:22");
+        LocalDateTime localDateTime = LocalDateTime.parse("2021-06-21T22:22:22");
+        //int year = localDate.get(ChronoField.YEAR);
+        localDateTime.toLocalDate();
+        localDateTime.toLocalTime();
+        LocalDateTime ldt = localTime.atDate(localDate);
+        LocalDateTime ldt2 = localDate.atTime(localTime);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        LocalDateTime ldt3 = LocalDateTime.parse("20210101222222", dtf);
 
 
+        ZoneId zoneId = TimeZone.getDefault().toZoneId();
+        ZonedDateTime zonedDateTime = ldt3.atZone(zoneId);
+
+        System.out.println(zonedDateTime.format(dtf));
+        System.out.println(ldt3.format(dtf));
     }
 
 }
