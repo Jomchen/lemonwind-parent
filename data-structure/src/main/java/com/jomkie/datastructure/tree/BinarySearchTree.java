a34e79ce6ff6d73e841dbab605cee28a2fdfd1a3
Jomkie <937574740@qq.com>
2021-08-09 23:06:41 +0800

    增加学习内容

--- data-structure/src/main/java/com/jomkie/datastructure/tree/BinarySearchTree.java
+++ data-structure/src/main/java/com/jomkie/datastructure/tree/BinarySearchTree.java
@@ -81,6 +81,117 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         return false;
     }
 
+    public int height() {
+        return height(root);
+    }
+    private int height(Node<E> node) {
+        if (node == null) { return 0; }
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+    public int height2() {
+        if (null == root) { return 0; }
+
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.add(root);
+        int currentHeight = 0;
+        int numberOfCurrentLayer = queue.size();
+        while ( ! queue.isEmpty()) {
+            Node<E> node = queue.poll();
+            if (null != node.left) {
+                queue.add(node.left);
+            }
+            if (null != node.right) {
+                queue.add(node.right);
+            }
+
+            -- numberOfCurrentLayer;
+            if (numberOfCurrentLayer == 0) {
+                currentHeight ++;
+                numberOfCurrentLayer = queue.size();
+            }
+        }
+
+        return currentHeight;
+    }
+
+    public boolean isComplete() {
+        if (null == root) { return false; }
+        boolean result = false;
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean existLeaf = false;
+        while ( ! queue.isEmpty()) {
+            Node<E> node = queue.poll();
+            Node<E> left = node.left;
+            Node<E> right = node.right;
+
+            if (existLeaf && ! node.isLeaf()) { return false; }
+
+            if (left != null) {
+                queue.add(left);
+            } else if (right != null) {
+                return false;
+            }
+
+            if (right != null) {
+                queue.add(right);
+            } else { // right = null
+                existLeaf = true;
+            }
+
+
+            /*这块代码不如以上优秀
+                if (node.hasTwoChildren()) {
+                queue.add(left);
+                queue.add(right);
+            } else if (null == left && null != right) {
+                return false;
+            } else {
+                // 左右都为空，左不为空且右为空
+                // 这两种情况出现一种，之后的元素度必须为 0 才是完全二叉树
+                existLeaf = true;
+                if (left != null) {
+                    queue.add(left);
+                }
+            }*/
+        }
+
+        return true;
+    }
+
+    /** 翻转二叉树，任何一种遍历方式都可以翻转 */
+    public void invertTree() {
+        if (null == root) { return; }
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.add(root);
+
+        while ( ! queue.isEmpty()) {
+            Node<E> node = queue.poll();
+            Node<E> temporary = node.left;
+            node.left = node.right;
+            node.right  = temporary;
+
+            if (null != node.left) { queue.add(node.left); }
+            if (null != node.right) { queue.add(node.right); }
+        }
+    }
+
+    public void invertTree2() {
+        invertTree2(root);
+    }
+    // 前序遍历方式翻转
+    public void invertTree2(Node<E> node) {
+        if (null == node) { return; }
+
+        Node<E> tem = node.left;
+        node.left = node.right;
+        node.right = tem;
+
+        invertTree2(node.left);
+        invertTree2(node.right);
+    }
+
     /** 前序遍历 Preorder Traversal */
     public void preorderTraversal(Consumer<E> consumer) {
         preorderTraversalTool(root, consumer);
@@ -205,6 +316,13 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         public Node(E element, Node<E> parent) {
             this.element = element;
             this.parent = parent;
+        }
+
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
+        public boolean hasTwoChildren() {
+            return left != null && right != null;
         }
     }
 
--- data-structure/src/main/java/com/jomkie/datastructure/tree/TestBinarySearchTree.java
+++ data-structure/src/main/java/com/jomkie/datastructure/tree/TestBinarySearchTree.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class TestBinarySearchTree {
 
     public static void main(String[] args) {
-        test00();
+        //test00();
+        //test01();
+        test02();
     }
 
     /** 测试二叉树打印 */
@@ -21,7 +23,38 @@ public class TestBinarySearchTree {
         //bst.preorderTraversal(System.out::println);
         //bst.inorderTraversal(System.out::println);
         //bst.postorderTraversal(System.out::println);
-        bst.levelOrderTraversal(5, System.out::println);
+        //bst.levelOrderTraversal(5, System.out::println);
+        //System.out.println("高度" + bst.height2());
+        System.out.println(bst.isComplete());
+    }
+
+    /** 测试是否是完全二叉树 */
+    public static void test01() {
+        BinarySearchTree bst = new BinarySearchTree();
+        // false
+        //List<Integer> dataList = Arrays.asList(7, 4, 9, 2, 5, 8, 11, 1, 3, 10, 12);
+
+        // true
+        List<Integer> dataList = Arrays.asList(7, 4, 9, 2, 5);
+
+        // false
+        //List<Integer> dataList = Arrays.asList(7, 4, 9, 2, 1);
+        //System.out.println(bst.isComplete());
+
+        dataList.forEach(bst::add);
+        BinaryTrees.println(bst);
+        bst.invertTree();
+        BinaryTrees.println(bst);
+    }
+
+    /** 翻转二叉树 */
+    public static void test02() {
+        BinarySearchTree bst = new BinarySearchTree();
+        List<Integer> dataList = Arrays.asList(7, 4, 9, 2, 5);
+        dataList.forEach(bst::add);
+        BinaryTrees.println(bst);
+        bst.invertTree();
+        BinaryTrees.println(bst);
     }
 
 }
