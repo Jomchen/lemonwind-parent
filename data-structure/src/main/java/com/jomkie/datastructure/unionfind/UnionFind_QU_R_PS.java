ba68a2ba79f2bb8878c8517dee1b212940da67db
Jomkie <937574740@qq2.com>
2021-10-11 16:45:13 +0800

    修改错误的注释

--- data-structure/src/main/java/com/jomkie/datastructure/unionfind/UnionFind_QU_R_PS.java
+++ data-structure/src/main/java/com/jomkie/datastructure/unionfind/UnionFind_QU_R_PS.java
@@ -2,7 +2,7 @@ package com.jomkie.datastructure.unionfind;
 
 /**
  * 基于 UnionFind_QU_R 的路径分裂（Path Spliting）
- * 在 find(v) 时，让v以上直到根的所有每个节点都指向其祖父节点（即父亲的父亲）
+ * 在 find(v) 时，让v以上直到根的所有每个节点都指向对应的祖父节点（即父亲的父亲）
  * @author Jomkie
  * @since 2021-10-10 18:49:38
  */
