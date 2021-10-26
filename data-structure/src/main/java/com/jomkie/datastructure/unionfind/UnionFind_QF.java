1cd5539733de4d696d537608eba4606012dead0d
Jomkie <937574740@qq2.com>
2021-10-26 17:42:26 +0800

    修改说明

--- data-structure/src/main/java/com/jomkie/datastructure/unionfind/UnionFind_QF.java
+++ data-structure/src/main/java/com/jomkie/datastructure/unionfind/UnionFind_QF.java
@@ -2,7 +2,7 @@ package com.jomkie.datastructure.unionfind;
 
 /**
  * QuickFind 查询快，合并慢
- * 树的高度最多只有两级，相当于是一个集合的所有元素直接挂在另一个根节点下
+ * 树的高度最多只有两级，相当于是一个集合的所有元素直接挂在另一个集合的根节点下
  * 查询快，但是在合并时比较耗性能
  * @author jomkie
  * @since 2021-10-08 23:02:57
@@ -22,7 +22,7 @@ public class UnionFind_QF extends UnionFind {
 	@Override
 	public void union(int v1, int v2) {
 		// 此种相当于每个元素存储的就是索引所属集合的根节点
-		// 得到 v2 的根节点 root1，将 v1 所在集合的所有元素都直接挂在 p2 下 
+		// 得到 v1 的根节点 root1，得到 v2 的根节点 root2，把 root1 这个集合的所有元素直接挂在 root2 下
 		int root1 = find(v1);
 		int root2 = find(v2);
 		if (root1 == root2) { return; }
