bd088d1e5f62f18233a707b5dc437f36223e10f1
Jomkie <937574740@qq2.com>
2021-11-09 16:58:38 +0800

    add the new features of JDK8

---
+++ jomkie-test/src/main/java/com/jomkie/test/AllTest.java
@@ -0,0 +1,47 @@
+package com.jomkie.test;
+
+import com.jomkie.common.entity.bean.JoUser;
+
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class AllTest {
+
+    public static void main(String[] args) {
+        Optional<Integer> sumData = IntStream.range(0, 10).boxed().reduce(Integer::sum);
+        Integer sumData2 = IntStream.range(0, 10).boxed().reduce(0, Integer::sum);
+        Optional<Integer> maxData = IntStream.range(0, 10).boxed().reduce(Integer::max);
+
+        // 年龄分组，取得组中ID最大的对象
+        Map<Integer, Optional<JoUser>> maxIdMap = Stream.iterate(0, x -> x + 1).limit(10)
+                .map(i -> new JoUser(i, String.valueOf(i), i % 3, "北京" + i, new Date()))
+                .collect(groupingBy(JoUser::getAge, maxBy(Comparator.comparing(JoUser::getId))));
+
+        // 年龄分组，取得组中ID最大的对象
+        Map<Integer, JoUser> maxIdMap2 = Stream.iterate(0, x -> x + 1).limit(10)
+                .map(i -> new JoUser(i, String.valueOf(i), i % 3, "北京" + i, new Date()))
+                .collect(
+                        groupingBy(
+                                JoUser::getAge,
+                                collectingAndThen(maxBy(Comparator.comparing(JoUser::getId)), Optional::get)
+                        )
+                );
+
+        // 年龄分组，结果映射为ID并收集为集合
+        Map<Integer, List<Integer>> sumIdMap = Stream.iterate(0, x -> x + 1).limit(10)
+                .map(i -> new JoUser(i, String.valueOf(i), i % 3, "北京" + i, new Date()))
+                .collect(
+                        groupingBy(JoUser::getAge, mapping(user -> user.getId(), toList()))
+                );
+
+        // 分区，分区组的 KEY 是布尔型
+        Map<Boolean, List<JoUser>> partitionMap = Stream.iterate(0, x -> x + 1).limit(10)
+                .map(i -> new JoUser(i, String.valueOf(i), i % 3, "北京" + i, new Date()))
+                .collect(partitioningBy(user -> user.getId() > 5, toList()));
+        
+    }
+
+}
