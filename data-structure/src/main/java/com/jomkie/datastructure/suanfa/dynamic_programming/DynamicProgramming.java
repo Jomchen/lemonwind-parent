d60fa451012f125e78347dd154f62e2a17de4d7b
Jomkie <937574740@qq.com>
2021-11-23 22:44:23 +0800

    学习进度

---
+++ data-structure/src/main/java/com/jomkie/datastructure/suanfa/dynamic_programming/DynamicProgramming.java
@@ -0,0 +1,62 @@
+package com.jomkie.datastructure.suanfa.dynamic_programming;
+
+/**
+ * 动态规划
+ * @author Jomkie
+ * @since 2021-11-23 21:37:44
+ */
+public class DynamicProgramming {
+
+    public static void main(String[] args) {
+        System.out.println(coinChange());
+    }
+
+    /**
+     * 零钱兑换-找补个数最小
+     * @author Jomkie
+     * @since 2021-11-23 21:38:20
+     * @return void
+     */
+    public static int coinChange() {
+        // 20, 20, 1
+//        int money = 41;
+//        int[] faces = {25, 20, 5, 1};
+//        return coinChangeTool(faces, money);
+
+        // 7
+//        int money = 19;
+        // 3
+        int money = 41;
+        int[] faces = {25, 20, 5, 1};
+        int[] dp = new int[money + 1];
+        for (int i = 0; i < faces.length; i++) {
+            if (faces[i] <= money) { dp[faces[i]] = 1; }
+        }
+        return coinChangeTool2(faces, dp, money);
+    }
+    /** 暴力递归 */
+    private static int coinChangeTool(int[] faces, int money) {
+        if (money < 1) { return Integer.MAX_VALUE; }
+        if (money == 25 || money == 20 || money == 5 || money == 1) { return 1; }
+        int min1 = Math.min(coinChangeTool(faces, money - 25), coinChangeTool(faces, money - 20));
+        int min2 = Math.min(coinChangeTool(faces, money - 5), coinChangeTool(faces, money - 1));
+        return Math.min(min1, min2) + 1;
+    }
+    /** 记忆化搜索-优化 */
+    private static int coinChangeTool2(int[] faces, int[] dp, int money) {
+        if (money < 1) { return Integer.MAX_VALUE; }
+        if (dp[money] == 0) {
+            int min1 = Math.min(coinChangeTool2(faces, dp, money - 25), coinChangeTool2(faces, dp, money - 20));
+            int min2 = Math.min(coinChangeTool2(faces, dp, money - 5), coinChangeTool2(faces, dp, money - 1));
+            dp[money] = Math.min(min1, min2) + 1;
+        }
+        return dp[money];
+    }
+    private static int coinChangeTool3(int[] faces, int n) {
+        if (n < 1) { return -1; }
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+
+        }
+    }
+}
