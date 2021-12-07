1b9a26a3c023fc10603bbbb0a589c2a2369395f9
Jomkie <937574740@qq.com>
2021-12-07 22:58:46 +0800

    布点隆过滤器

--- data-structure/src/main/java/com/jomkie/datastructure/bloomfilter/BloomFilter.java
+++ data-structure/src/main/java/com/jomkie/datastructure/bloomfilter/BloomFilter.java
@@ -26,8 +26,12 @@ public class BloomFilter<T> {
     /** 二进制向量的长度（一共有多少个二进制位）*/
     private int bitSize;
 
-    /** 二进制向量 */
+    /** 二进制向量，这里存放有区别，如图示： */
+    /** 63-0, 127-64, 191-128, ...,... */
     private long[] bits;
+
+    /** Hash函数的个数 */
+    private int hashSize;
 
     /**
      * @param n 数据规模
@@ -39,7 +43,15 @@ public class BloomFilter<T> {
         }
 
         double ln2 = Math.log(2);
+        // 求出二进制向量的长度
+        bitSize = (int) (-(n * Math.log(p)) / (ln2 * ln2));
+        // 求出哈希函数的个数
+        hashSize = (int) (bitSize * ln2  / n);
+        // bits 数组的长度
+        bits = new long[(bitSize  + Long.SIZE - 1)/ Long.SIZE];
 
+        System.out.println(bitSize);
+        System.out.println(hashSize);
     }
 
 
@@ -47,8 +59,48 @@ public class BloomFilter<T> {
      * 添加元素
      * @param value
      */
-    public void put(T value) {
+    public boolean put(T value) {
+        nullCheck(value);
 
+        int hash1 = value.hashCode();
+        int hash2 = hash1 >>> 16;
+
+        boolean result = false;
+        for (int i = 1; i <= hashSize; i++) {
+            int combinedHash = hash1 + (i * hash2);
+            if (combinedHash < 0) {
+                combinedHash = ~combinedHash;
+            }
+            // 生成一个二进位的索引，并设置相应位置为 1
+            int index = combinedHash % bitSize;
+            // 设置 index 位置的二进位为 1
+            if (set(index)) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 设置 index 位置的二进制位为 1
+     * @param index
+     */
+    private boolean set(int index) {
+        long value = bits[index / Long.SIZE];
+        int bitValue = 1 << (index % Long.SIZE);
+        bits[index / Long.SIZE] = value | bitValue;
+        return (value & bitValue) == 0;
+    }
+
+    /**
+     * 查看 index 二进制位置的值
+     * @param index 索引位置
+     * 如果为 1 返回 true，否则返回 false
+     */
+    private boolean get(int index) {
+        long data = bits[index / Long.SIZE];
+        return (data & (1 << (index % Long.SIZE))) != 0;
     }
 
     /**
@@ -57,7 +109,42 @@ public class BloomFilter<T> {
      * @return
      */
     public boolean contains(T value) {
-        return false;
+        nullCheck(value);
+
+        int hash1 = value.hashCode();
+        int hash2 = hash1 >>> 16;
+
+        for (int i = 1; i <= hashSize; i++) {
+            int combinedHash = hash1 + (i * hash2);
+            if (combinedHash < 0) {
+                combinedHash = ~combinedHash;
+            }
+            // 生成一个二进位的索引
+            int index = combinedHash % bitSize;
+            if (! get(index)) { return false; }
+        }
+
+        return true;
+    }
+
+    private void nullCheck(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("the value is null");
+        }
+    }
+
+    public static void main(String[] args) {
+//        BloomFilter<Integer> bf = new BloomFilter<>(1_000_000, 0.01);
+//        for (int i = 0; i <= 50; i++) {
+//            bf.put(i);
+//        }
+//        for (int i = 0; i <= 50; i++) {
+//            System.out.println(bf.contains(i));
+//        }
+
+        BloomFilter<String> bf2 = new BloomFilter<>(1_000_000, 0.01);
+        bf2.put("123123");
+        System.out.println(bf2.contains("123123"));;
     }
 
 }
