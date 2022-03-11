0a38bd158d8849088fbd468a46fd53512df3cb16
Jomkie <937574740@qq2.com>
2022-03-11 15:27:23 +0800

    修改字符串逻辑

--- jomkie-test/src/main/java/com/jomkie/test/suanfa/CharSequnceTest.java
+++ jomkie-test/src/main/java/com/jomkie/test/suanfa/CharSequnceTest.java
@@ -14,20 +14,18 @@ public class CharSequnceTest {
     	check(text, pattern);
 
         int boundLen = text.length() - pattern.length();
-        for (int ti = 0; ti < text.length(); ti++) {
-            int pi = 0;
-            while (pi < pattern.length() && ti - pi <= boundLen) {
-                if (text.charAt(ti + pi) == pattern.charAt(pi)) {
-                    pi++;
-                } else {
-                    break;
-                }
-            }
+        for (int ti = 0; ti <= boundLen; ti++) {
+        	int pi = 0;
+        	while (pi < pattern.length()) {
+        		if (text.charAt(ti + pi) == pattern.charAt(pi)) {
+        			pi++;
+				} else {
+        			break;
+				}
+			}
 
-            if (pi == pattern.length()) {
-                return ti;
-            }
-        }
+        	if (pi == pattern.length()) { return ti; }
+		}
 
         return -1;
     }
