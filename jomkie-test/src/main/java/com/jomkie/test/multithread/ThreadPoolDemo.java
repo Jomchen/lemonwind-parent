8c27afacdf536855ee768c5aa9b40beade4367f6
Jomkie <937574740@qq.com>
2022-05-17 21:46:15 +0800

    增加了一些类

---
+++ jomkie-test/src/main/java/com/jomkie/test/multithread/ThreadPoolDemo.java
@@ -0,0 +1,17 @@
+package com.jomkie.test.multithread;
+
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+
+    public static void main(String[] args) {
+        // 创建固定线程数的线程池
+//        Executors.newFixedThreadPool();
+        // 一个任务一个任务执行，一池一线程
+//        Executors.newSingleThreadExecutor()
+        // 线程池根据需求创建线程，可扩容，遇强则强（无限扩容容易内存溢出）
+//        Executors.newCachedThreadPool();
+    }
+
+
+}
