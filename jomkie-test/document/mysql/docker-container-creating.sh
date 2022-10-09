35b0350dc55e41922ccc51c982fccd583bb27803
Jomkie <937574740@qq2.com>
2022-10-09 14:28:13 +0800

    修改

--- jomkie-test/document/mysql/docker-container-creating.sh
+++ jomkie-test/document/mysql/docker-container-creating.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Author: Jomkie
 
-
 containerName=mysql00
 baseDir=/opt/docker-container/mysql/${containerName}
 logDir=${baseDir}/logs
@@ -23,6 +22,7 @@ fi
   # 挂载数据 -v xxx:/var/lib/mysql \
 
 docker run \
+  --privileged=true \
   --name ${containerName} \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD=root \
